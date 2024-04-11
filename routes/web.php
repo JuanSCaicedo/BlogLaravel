35988583f9313647b2509f0f50ccb403116b2792
JuanSCaicedo <sebanchit@gmail.com>
2024-04-11 18:44:26 -0500

    mostrar listado de posts

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
 Route::middleware([
     'auth:sanctum',
