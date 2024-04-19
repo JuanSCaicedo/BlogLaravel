662614b340e437901284e1ae87ba6697bd2caa4c
JuanSCaicedo <sebanchit@gmail.com>
2024-04-19 18:42:11 -0500

    crear nuevo post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -8,6 +8,8 @@ use App\Models\Post;
 use App\Models\Category;
 use App\Models\Tag;
 
+use App\Http\Requests\StorePostRequest;
+
 class PostController extends Controller
 {
     /**
@@ -32,9 +34,16 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
-        //
+        $post = Post::create($request->all());
+
+        if($request->tags)
+        {
+            $post->tags()->attach($request->tags);
+        }
+
+        return redirect()->route('admin.posts.edit', $post);
     }
 
     /**
--- resources/views/admin/posts/partials/form.blade.php
+++ resources/views/admin/posts/partials/form.blade.php
@@ -1,20 +1,22 @@
+<input type="hidden" name="user_id" value="{{auth()->user()->id}}">
+
 <div class="form-group mb-2">
     <label for="name">Nombre</label>
     <input name="name" id="name" type="text" class="form-control"
-        value="{{ isset($post) ? $post->name : old('name') }}" placeholder="Ingrese el nombre del post">
+        value="{{ old('name', isset($post) ? $post->name : '') }}" placeholder="Ingrese el nombre del post">
 
     @error('name')
-        <span class="text-danger">{{ $message }}</span>
+        <small class="text-danger">{{ $message }}</small>
     @enderror
 </div>
 
 <div class="form-group mb-2">
     <label for="slug">Slug</label>
     <input readonly name="slug" id="slug" type="text" class="form-control"
-        value="{{ isset($post) ? $post->slug : old('slug') }}" placeholder="Ingrese el slug del post">
+        value="{{ old('slug', isset($post) ? $post->slug : '') }}" placeholder="Ingrese el slug del post">
 
     @error('slug')
-        <span class="text-danger">{{ $message }}</span>
+        <small class="text-danger">{{ $message }}</small>
     @enderror
 </div>
 
@@ -24,15 +26,14 @@
     <select name="category_id" id="category_id" class="form-control">
         <option value="" selected disabled>-- Seleccione --</option>
         @foreach ($categories as $key => $category)
-            <option value="{{ $key }}"
-                @if (isset($post) && $post->category == $key) selected @elseif (old('category_id') == $key) selected @endif>
+            <option value="{{ $key }}" @if (old('category_id') == $key || (isset($post) && $post->category == $key)) selected @endif>
                 {{ $category }}
             </option>
         @endforeach
     </select>
 
     @error('category_id')
-        <span class="text-danger">{{ $message }}</span>
+        <small class="text-danger">{{ $message }}</small>
     @enderror
 </div>
 
@@ -41,32 +42,53 @@
 
     @foreach ($tags as $tag)
         <label class="mr-2">
-            <input type="checkbox" id="{{ $tag->id }}" name="tags[]" value="{{ $tag->id }}">
+            <input type="checkbox" id="{{ $tag->id }}" name="tags[]" value="{{ $tag->id }}"
+                {{ in_array($tag->id, old('tags', [])) ? 'checked' : '' }}>
             <label for="{{ $tag->id }}">{{ $tag->name }}</label>
         </label>
     @endforeach
+
+    @error('tags')
+        <br>
+        <small class="text-danger">{{ $message }}</small>
+    @enderror
 </div>
 
 <div class="form-group">
     <p class="font-weight-bold">Estado</p>
 
     <label class="mr-2">
-        <input type="radio" name="status" value="1" checked>
+        <input type="radio" id="status_borrador" name="status" value="1" checked
+            {{ old('status') === null && !isset($post) ? 'checked' : old('status', isset($post) && $post->status == 1 ? 'checked' : '') }}>
         Borrador
     </label>
 
     <label>
-        <input type="radio" name="status" value="2">
+        <input type="radio" id="status_publicado" name="status" value="2"
+            {{ old('status', isset($post) && $post->status == 2 ? 'checked' : '') }}>
         Publicado
     </label>
+
+    @error('status')
+        <br>
+        <small class="text-danger">{{ $message }}</small>
+    @enderror
 </div>
 
 <div class="form-group">
     <label for="extract">Extracto:</label>
-    <textarea name="extract" id="extract" class="form-control"></textarea>
+    <textarea name="extract" id="extract" class="form-control">{{ old('extract', isset($post) ? $post->extract : '') }}</textarea>
+
+    @error('extract')
+        <small class="text-danger">{{ $message }}</small>
+    @enderror
 </div>
 
 <div class="form-group">
     <label for="body">Cuerpo del post:</label>
-    <textarea name="body" id="body" class="form-control"></textarea>
+    <textarea name="body" id="body" class="form-control">{{ old('body', isset($post) ? $post->body : '') }}</textarea>
+
+    @error('body')
+        <small class="text-danger">{{ $message }}</small>
+    @enderror
 </div>
