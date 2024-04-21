067b9cde9d61345d0eb3a66934ae0d07bbdb9862
JuanSCaicedo <sebanchit@gmail.com>
2024-04-21 16:05:02 -0500

    agregando slug a posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,11 @@ class Post extends Model
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     //Relacion uno a muchos inversa
     public function user()
     {
